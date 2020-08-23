cac0136364df4782d34dc904bb0200d95ea92e1c
Anton Krivosheev <krivosheev777@gmail.com>
2020-08-23 21:38:08 +0300

    подключаем api

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,14 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+'use strict'
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+fetch('http://api.openweathermap.org/data/2.5/weather?id=536203&appid=eaa9becf88b7e7f447444838b2264952')
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector('.weather__city').textContent = data.name;
+        document.querySelector('.weather__degree').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        document.querySelector('.weather__disclaimer').textContent = data.weather[0]['description'];
+        document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        // catch any errors
+    });
