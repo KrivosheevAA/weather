fe873b81054e7cba9c3c1c958df1e4f3808488b5
Anton Krivosheev <krivosheev777@gmail.com>
2020-08-24 23:27:21 +0300

    пробуем с 3 городами

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,25 @@
 'use strict'
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=536203&appid=eaa9becf88b7e7f447444838b2264952')
+fetch('http://api.openweathermap.org/data/2.5/group?id=524901,703448,498817&units=metric&appid=eaa9becf88b7e7f447444838b2264952')
     .then(function (resp) { return resp.json() })
     .then(function (data) {
         console.log(data);
-        document.querySelector('.weather__city').textContent = data.name;
-        document.querySelector('.weather__degree').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-        document.querySelector('.weather__disclaimer').textContent = data.weather[0]['description'];
-        document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+          let weatherCity = document.querySelector('.weather__city');
+          let weatherDegree = document.querySelector('.weather__degree')
+          weatherCity.addEventListener('change', (e) => {
+            for (let key in list) {
+              console.log(list);
+            }
+
+          })
+          weatherDegree.innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+
+        // document.querySelector('.weather__city').value.textContent = data.name;
+        // document.querySelector('.weather__degree').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        // document.querySelector('.weather__disclaimer').textContent = data.weather[0]['description'];
+        // document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
     })
     .catch(function () {
         // catch any errors
     });
+
